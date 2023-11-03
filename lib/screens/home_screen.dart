70e0e76258bf46055b5b8e2c8b3892fddf46c0f5
j1eunkim <jijinn0517@naver.com>
2023-11-04 01:26:01 +0900

    commit 2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,7 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
             Flexible(
